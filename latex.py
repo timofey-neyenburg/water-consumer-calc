1b23efc3174274ae3584ebe6e5d3269531767813
Тимофей Нейенбург <neyenburgz@mail.ru>
2025-05-01 15:08:43 +0200

    latex

--- latex.py
+++ latex.py
@@ -1,3 +1,4 @@
+import datetime
 from fileinput import filename
 import logging
 from os import PathLike
@@ -6,75 +7,172 @@ import threading
 from pathlib import Path
 
 from data import PROJECT_ROOT
+from mathematics import WaterConsumerNorms
 from settings import app_logger
 
 
-DOC_TEMPLATE = r"""
-\documentclass{article}
-\usepackage[T2A]{fontenc}
-\usepackage[russian,english]{babel}
-\usepackage[margin=1.5cm]{geometry}
 
-\begin{document}
+def _build_document_top(images_path: str = './', header_image_name: str = "header.png"):
+    txt = (
+        "\\documentclass{article} \n"
+        "\\usepackage[T2A]{fontenc} \n"
+        "\\usepackage[russian,english]{babel} \n"
+        "\\usepackage[margin=1.5cm]{geometry} \n"
+        "\\usepackage{multirow} \n"
+        "\\usepackage{times} \n"
+        "\\usepackage{graphicx} \n"
+        "\\graphicspath{ {" + images_path + "} } \n\n"
+        "\\includegraphics[width=18cm, height=5cm]{" + header_image_name + "} \n\n"
+        "\\begin{document} \n"
+    )
 
-    \section*{\textbf{Общий секундный расход:}} \\
-    \textbf{Секундный расход воды различными приборами, обслуживающими разных водопотребителей, определяется по формуле (2) СП30.13330.2020} \\
-    \\
-        $q^{tot} = 5 \cdot q_0 \cdot \alpha$ \\
-    \\
-    \textbf{Расчет начинаем с определения вероятности действия приборов различными потребителями:} \\
-    \textbf{Формула (3) СП30.13330.2020 По таблице Б.1 СП30.13330.2020 находим значение коэффициента \alpha.} \\
-    \\
-        $P = q^{tot}_{hr,u} \cdot U / (q_0 \cdot N \cdot 3600)$ \\
-    \\
-    <Название потребителя> \\
-    <Название потребителя: приборы> \\
-    \\
-        ... ФОРМУЛЫ ... \\
-    \\
-    \section*{Максимальный часовой расход воды (стоков)}
-    Максимальный часовой расход воды (стоков) $q_{hr} (q^{tot}_{hr}, q^{h}_{hr}, q^{h}_{cr})$ м3, следует определять по формуле: \\
-    \\
-        $q_{hr} = 0.005 \cdot q_{0, hr} \cdot \alpha_{hr}$ \\
-    \\
-    Вероятность использования санитарно-технических приборов $P_{hr}$ для системы в целом следует определять по формуле: \\
-    \\
-        $P_{hr} = (3600 \cdot P \cdot q_0) / (q_{0, hr})$ \\
-    \\
-    <Название потребителя> \\
-    <Название потребителя: приборы> \\
-    \\
-        ... ФОРМУЛЫ ... \\
-    \\
-    \section*{Средний часовой расход воды}
-    Средний часовой расход воды $q_{T} (q_{T}^{tot}, q_{T, h}, q_{T, c})$, м3, за расчетное время водопотребления (сутки, смена) Т, ч, следует определять по формуле: \\
-    \\
-        $q_{T} = \frac{\sum_1^i{q_{u,i}\cdot U_i}}{1000 \cdot T}$ \\
-    \\
-    <Название потребителя> \\
-    \\
-        ... ФОРМУЛЫ ... \\
-    \\
-    Расход тепла $Q_{T, h}  (Q_{h, hr})$, кВт, на приготовление горячей воды с учетом потерь тепла подающими и циркуляционными трубопроводами $Q_{h, t}$ следует определять: \\
-    а) в течение среднего часа \\
-    \\
-        $Q_{T}^h = 1.16 \cdot q_T^h \cdot (t^h - t^c) + Q^{ht}$ \\
-    \\
-    б) в течение часа максимального водопотребления \\
-    \\
-        $Q_{hr}^{t} = 1.16 \cdot q_{hr}^h \cdot (t^h - t^c) + Q^{ht}$ \\
-    \\
-    <Название потребителя> \\
-    \\
-        ... ФОРМУЛЫ ... \\
-    \\
-    \section*{\textbf{Общий суточный расход:}} \\
-    \\
-        ... ФОРМУЛЫ ... \\
-    \\
+    return txt
 
-\end{document}
+
+def _build_doc_header(project_name: str) -> str:
+    txt = (
+        (
+            "\\begin{center} \n"
+            "\\section*{<project_name>}\\\\ \n"
+            "\\end{center} \n"
+            "\\begin{center} \n"
+            "\\section*{Расчет расходов воды и стоков.}\\\\\\\\ \n"
+            "\\end{center} \n"
+            "Согласно задания на проектирование выполняем расчет расходов воды по СНиП 2.04.01-85 «Внутренний водопровод и канализация зданий».\\\\ \n\n"
+        )
+        .replace("<project_name>", project_name)
+    )
+
+    return txt
+
+
+def _build_document_objects_list(objects: list[tuple[str, int, str]]) -> str:
+    txt = ""
+    for ind, (name, num_of_measurers, measurer) in enumerate(objects):
+        txt += (
+            f"{ind+1}. {name} - {num_of_measurers} {measurer} \\\\ \n"
+        )
+    txt += "\n"
+    return txt
+
+
+def _build_objects_info_table(objects: list[WaterConsumerNorms]) -> str:
+    # https://www.tablesgenerator.com/
+
+    table = """
+\\begin{table}[h] \n
+\\begin{tabular}{|m{1cm}|m{1cm}|m{1cm}|m{1cm}|m{1cm}|m{1cm}|m{1cm}|m{1cm}|m{1cm}|} \n
+\\hline \n
+    \\multicolumn{1}{|p{3cm}|}{ \n
+        \\multirow{3}{3cm}{Водопотребители} \n
+    }& \n
+    \\multicolumn{1}{p{1cm}|}{ \n
+        \\multirow{3}{1cm}{Изме ритель} \n
+    }& \n
+    \\multicolumn{1}{|p{1cm}|}{ \n
+        \\multirow{3}{1cm}{Коли чество потре бите лей} \n
+    }& \n
+    \\multicolumn{4}{p{4cm}|}{Норма расхода воды, л}& \n
+    \\multicolumn{2}{p{5cm}|}{Расход воды прибором, л/с (л/ч)} \\\\ \n
+\\cline{4-9} \n
+    \\multicolumn{1}{|p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{} & \n
+    \\multicolumn{2}{p{2cm}|}{В сутки наибольшего потребления} & \n
+    \\multicolumn{2}{p{2cm}|}{В час наибольшего потребления} & \n
+    \\multicolumn{1}{p{3cm}|}{ \n
+        \\multirow{2}{3cm}{Общий (холодной и горячей) $q^{tot}_o$ ($q^{tot}_{o, hr}$)} \n
+    } & \n
+    \\multicolumn{1}{p{2cm}|}{ \n
+        \\multirow{2}{2cm}{Холодной или горячей $q^c_o$, $q^h_o$, $q^c_{o, hr}$, $q^h_{o, hr}$} \n
+    } \\\\ \n
+\\cline{4-7} \n
+    \\multicolumn{1}{|p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{Общая (в том числе горя чей) $q^{tot}_u$} & \n
+    \\multicolumn{1}{p{1cm}|}{Горя чей $q^h_u$} & \n
+    \\multicolumn{1}{p{1cm}|}{Общая (в том числе горя чей) $q^{tot}_{hr, u}$} & \n
+    \\multicolumn{1}{p{1cm}|}{Горя чей $q^{h}_{hr, u}$} & \n
+    \\multicolumn{1}{p{1cm}|}{} & \n
+    \\multicolumn{1}{p{1cm}|}{} \\\\ \n
+\\hline
 """
+
+    table_bottom = """
+\\end{tabular} \n
+\\end{table}\\textbf{} \n\n
+"""
+
+    for obj in objects:
+        table += """
+\\multicolumn{1}{|p{3cm}|}{<name>} & \n 
+\\multicolumn{1}{ p{1cm}|}{<>} & \n 
+\\multicolumn{1}{ p{1cm}|}{<>} & \n 
+\\multicolumn{1}{ p{1cm}|}{<>} & \n
+\\multicolumn{1}{ p{1cm}|}{<>} & \n
+\\multicolumn{1}{ p{1cm}|}{<>} & \n
+\\multicolumn{1}{ p{1cm}|}{<>} & \n
+\\multicolumn{1}{ p{3cm}|}{<>} & \n 
+\\multicolumn{1}{ p{2cm}|}{<>}  \n \\\\ 
+\\hline
+""".replace("<name>", obj.name)
+
+    table += table_bottom
+    
+    txt = (
+        "\\section*{Норма расхода воды}\\\\ \n"
+        + table
+    )
+    return txt
+
+
+def _build_sec_calculations() -> str:
+    txt = (
+        "\\begin{center} \n"
+        "\\section*{Общий секундный расход:} \n"
+        "\\end{center} \n"
+        "Секундный расход воды  различными  приборами, обслуживающими разных водопотребителей, определяется по формуле 2 СНиП 2.04.01-85:\\\\ \n"
+        "\\begin{center} \n"
+        "$q^{tot} = 5 \\cdot q_0 \\cdot \\varalpha$, л/с \n"
+        "\\end{center} \n"
+        "Расчет начинаем с определения вероятности действия приборов различными потребителями:\\\\ \n"
+    )
+    return txt
+
+
+def _build_document_end() -> str:
+    today = datetime.datetime.now().strftime("%m.%d.%Y")
+
+    txt = (
+        "\\vspace{2cm}\\\\ \n\n"
+        "\\begin{tabular}{p{400pt}l}\n"
+        "\\Large{Директор}     & \\Large{Исполнил}   \\\\ \n"
+        "                      &                     \\\\ \n"
+        "\\rule{40pt}{0.4pt}   & \\rule{40pt}{0.4pt} \\\\ \n"
+        f"                     & {today}г.           \\\\ \n"
+        "\\end{tabular}  \n\n"
+        "\\end{document} \n"
+    )
+
+    return txt
+
+
+def build_document_text(
+    project_name: str = "Предложение по застройке территории на правом берегу реки Иртыш в г.Омске.",
+    objects: list[tuple[str, int, str]] = [("Жилая часть", 14507, "чел.")],
+    norms: list[WaterConsumerNorms] = [],
+):
+    document = (
+        _build_document_top()
+        + _build_doc_header(project_name)
+        + _build_document_objects_list(objects)
+        + _build_objects_info_table(norms)
+        + _build_sec_calculations()
+        + _build_document_end()
+    )
+
+    return document
 
 
 def prepare_latex(filepath: str, use_thread: bool = True, **params):
@@ -87,6 +185,8 @@ def prepare_latex(filepath: str, use_thread: bool = True, **params):
 
     if not filepath.endswith(".tex"):
         filepath += ".tex"
+    
+    document_text = build_document_text()
 
     def _run():
         from subprocess import run, DEVNULL
@@ -106,25 +206,29 @@ def prepare_latex(filepath: str, use_thread: bool = True, **params):
 
         temp_tex_filepath = str(LATEX_OUTPUT_FILES_TEMP_DIR / f"{filename_without_extention}.tex")
         with open(temp_tex_filepath, "w", encoding="utf-8") as f:
-            f.write(format_string(DOC_TEMPLATE, params))
+            f.write(document_text)
 
         # command = ['pdflatex', '--interaction=nonstopmode', filepath]
         # command = ["C:\\Program Files\\MiKTeX\\miktex\\bin\\x64\\pdflatex.exe", '--interaction=nonstopmode', temp_tex_filepath]
         command = [
-            "C:\\Users\\neyen\\OneDrive\\Рабочий стол\\TinyTeX-1\\TinyTeX\\bin\\windows\\pdflatex.exe",
+            r".\bin\windows\pdflatex.exe",
             '--interaction=nonstopmode',
             temp_tex_filepath
         ]
+        print(command)
 
         app_logger.debug("starting latex compilation")
-        run(
+        r = run(
             command,
             stdout=stdout_,
             stderr=stderr_,
-            cwd=LATEX_OUTPUT_FILES_TEMP_DIR,
+            # cwd=LATEX_OUTPUT_FILES_TEMP_DIR,
+            cwd=r".\assets\tex-minimal",
             shell=True,
         )
         app_logger.debug("finished latex compilation")
+        print("ERROR" if r.returncode == 1 else "OK")
+        return
 
         app_logger.debug("moving pdf file")
         move(
@@ -146,94 +250,6 @@ def prepare_latex(filepath: str, use_thread: bool = True, **params):
         _run()
 
 
-def format_string(string: str, values: dict):
-    return string
-
-
-def build_document_text(
-    project_name: str = "Предложение по застройке территории на правом берегу реки Иртыш в г.Омске.",
-    objects: list[tuple[str, int, str]] = [("Жилая часть", 14507, "чел.")],
-):
-    txt = """
-\\documentclass{article} \n\
-\\usepackage[T2A]{fontenc} \n\
-\\usepackage[russian,english]{babel} \n\
-\\usepackage[margin=1.5cm]{geometry} \n\
-\\usepackage{multirow} \n\
-\\usepackage{times} \n\
-
-\\begin{document} \n"""
-
-    txt += (
-        (
-            "\\section*{<project_name>}\\\\ \n"
-            "\\section*{Расчет расходов воды и стоков.}\\\\\\\\ \n"
-            "Согласно задания на проектирование выполняем расчет расходов воды по СНиП 2.04.01-85 «Внутренний водопровод и канализация зданий».\\\\ \n"
-        )
-        .replace("<project_name>", project_name)
-    )
-
-    for ind, (name, num_of_measurers, measurer) in enumerate(objects):
-        txt += (
-            f"{ind+1}. {name} - {num_of_measurers} {measurer} \\\\ \n"
-        )
-    
-    txt += "\\section*{Норма расхода воды}"
-
-    # https://www.tablesgenerator.com/
-    # \\begin{tabularx}{\\linewidth}{lllllllll} \n\
-    txt += (
-        """
-\\begin{table}[h] \n\
-\\begin{tabular}{p{2cm} llllllll} \n\
-\\hline \n\
-\\multicolumn{1}{p{2cm}}{
-    \\multirow{3}{*}{Водопотребители}
-} &
-\\multicolumn{1}{l|}{
-    \\multirow{3}{*}{Измеритель}
-} &
-\\multicolumn{1}{l|}{
-    \\multirow{3}{*}{Количество потребителей}
-} & 
-\\multicolumn{4}{l|}{Норма расхода воды, л} &
-\\multicolumn{2}{l|}{Расход воды прибором, л/с (л/ч)} \\\\ 
-\\cline{4-9}  \n\
-\\multicolumn{1}{|l|}{} &
-\\multicolumn{1}{l|}{} & 
-\\multicolumn{1}{l|}{} & 
-\\multicolumn{2}{l|}{В сутки наибольшего потребления} & 
-\\multicolumn{2}{l|}{В час наибольшего потребления} & 
-\\multicolumn{1}{l|}{
-    \\multirow{2}{*}{Общий (холодной и горячей) $q^{tot}_o$ ($q^{tot}_{o, hr}$)}
-} & 
-\\multicolumn{1}{l|}{
-    \\multirow{2}{*}{Холодной или горячей $q^c_o$, $q^h_o$, $q^c_{o, hr}$, $q^h_{o, hr}$}
-} \\\\ 
-\\cline{4-7}  \n\
-\\multicolumn{1}{|l|}{} & 
-\\multicolumn{1}{l|}{} & 
-\\multicolumn{1}{l|}{} & 
-\\multicolumn{1}{l|}{Общая (в том числе горячей) $q^{tot}_u$} & 
-\\multicolumn{1}{l|}{Горячей $q^h_u$} & 
-\\multicolumn{1}{l|}{Общая (в том числе горячей) $q^{tot}_{hr, u}$} & 
-\\multicolumn{1}{l|}{Горячей $q^{h}_{hr, u}$} & 
-\\multicolumn{1}{l|}{} & 
-\\multicolumn{1}{l|}{} \\\\ 
-\\hline \n\
-  & & & & & & & & \n
-\\end{tabularx} \n\
-\\end{table} \n\
-"""
-    )
-
-    txt += "\\end{document} \n"
-
-    return txt
-
-
-
-# prepare_latex("C:\\Users\\neyen\\OneDrive\\Рабочий стол\\doc", use_thread=False)
-
 with open("C:\\Users\\neyen\\OneDrive\\Рабочий стол\\e.tex", "w", encoding="utf-8") as f:
     f.write(build_document_text())
+# prepare_latex("C:\\Users\\neyen\\OneDrive\\Рабочий стол\\e.tex", use_thread=False)
